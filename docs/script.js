e9626345167868edec3e850526dd98a02d2f0e3f
zeezaa <ziizaa123@gmail.com>
2023-03-19 01:26:53 +0200

    optimized date checking

--- docs/script.js
+++ docs/script.js
@@ -3,12 +3,9 @@ function checkDates()
 	var checks = document.getElementsByClassName('checkmark');
 	for(var i = 0; i < checks.length; ++i)
 	{
-		var text = checks[i].title;
-		var str = text.split(" ");
-		var ddmmyy = str[1].split(".");
-		var ISO = new Date(ddmmyy[2]+"-"+ddmmyy[1]+"-"+ddmmyy[0]);
+		var ISO = new Date(checks[i].dataset.modified);
 		var today = new Date();
-		if(today.getTime() > ISO.getTime()+ 1209600033) //2 weeks in ms
+		if(today.getTime() > ISO.getTime() + 1209600000) //2 weeks in ms
 		{
 			checks[i].innerHTML="&#x2718;";
 			checks[i].className="xmark";
