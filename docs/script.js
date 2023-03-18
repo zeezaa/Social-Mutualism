563dafbb1fdd0942d04c3f935189b980bf75054e
zeezaa <ziizaa123@gmail.com>
2023-03-19 00:48:03 +0200

    date checking, font provider change
    grammar fixes

--- docs/script.js
+++ docs/script.js
@@ -1,15 +1,32 @@
+function checkDates()
+{
+	var checks = document.getElementsByClassName('checkmark');
+	for(var i = 0; i < checks.length; ++i)
+	{
+		var text = checks[i].title;
+		var str = text.split(" ");
+		var ddmmyy = str[1].split(".");
+		var ISO = new Date(ddmmyy[2]+"-"+ddmmyy[1]+"-"+ddmmyy[0]);
+		var today = new Date();
+		if(today.getTime() > ISO.getTime()+ 1209600033) //2 weeks in ms
+		{
+			checks[i].innerHTML="&#x2718;";
+			checks[i].className="xmark";
+		}
+	}
+}
+
 function toggleSect(sect)
 {
 	var sib = sect.nextElementSibling;
-	var icon = sect.querySelector('.sectI');
 	if(sib.className=="closed")
 	{
 		sib.className="open";
-		icon.src="minus.svg"
+		sect.querySelector('.sectI').src="minus.svg";
 	}
 	else
 	{
 		sib.className="closed";
-		icon.src="plus.svg"
+		sect.querySelector('.sectI').src="plus.svg";
 	}
 }
